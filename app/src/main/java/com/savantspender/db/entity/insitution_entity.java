808ce22510dd4f6d4e66ea32b8b294f5c624503f
RileyPeter <rjwpeterson@csu.fullerton.edu>
2019-04-26 20:28:21 -0700

    Add files via upload

---
+++ app/src/main/java/com/savantspender/db/entity/insitution_entity.java
@@ -0,0 +1,12 @@
+package com.savantspender.db.entity;
+
+
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity
+public class insitution_entity {
+    @PrimaryKey
+    protected String id;
+    protected String name;
+}
