a7cdd29af88b1af6ef3c5b0cae2f2abf1badd466
amrazek <amrazek@hotmail.com>
2019-04-27 23:13:55 -0700

    add CataloggedEntity class

--- app/src/main/java/com/savantspender/db/entity/CataloggedEntity.java
+++ app/src/main/java/com/savantspender/db/entity/CataloggedEntity.java
@@ -1,32 +1,32 @@
-//package com.savantspender.db.entity;
-//
-//
-//import androidx.annotation.NonNull;
-//import androidx.room.Entity;
-//import androidx.room.ForeignKey;
-//import androidx.room.Index;
-//
-//@Entity(
-//        tableName = "catalogged",
-//        foreignKeys = {
-//                @ForeignKey(
-//                        parentColumns = "AccountId",
-//                        childColumns = "id",
-//                        entity = Account.class),
-//                @ForeignKey(
-//                        parentColumns = "TransactionId",
-//                        childColumns = "id",
-//                        entity = Transaction.class),
-//                @ForeignKey(
-//                        parentColumns = "TagId",
-//                        childColumns = "id",
-//                        entity = TagEntity.class
-//                )
-//        },
-//        indices = { @Index("TransactionId")},
-//        primaryKeys = {"AccountId", "TransactionId", "TagId"})
-//public class CataloggedEntity {
-//    public @NonNull String AccountId;
-//    public @NonNull String TransactionId;
-//    public int TagId;
-//}
+package com.savantspender.db.entity;
+
+
+import androidx.annotation.NonNull;
+import androidx.room.Entity;
+import androidx.room.ForeignKey;
+import androidx.room.Index;
+
+@Entity(
+        tableName = "catalogged",
+        foreignKeys = {
+                @ForeignKey(
+                        parentColumns = "AccountId",
+                        childColumns = "id",
+                        entity = AccountEntity.class),
+                @ForeignKey(
+                        parentColumns = "TransactionId",
+                        childColumns = "id",
+                        entity = TransactionEntity.class),
+                @ForeignKey(
+                        parentColumns = "TagId",
+                        childColumns = "id",
+                        entity = TagEntity.class
+                )
+        },
+        indices = { @Index("TransactionId")},
+        primaryKeys = {"AccountId", "TransactionId", "TagId"})
+public class CataloggedEntity {
+    public @NonNull String AccountId;
+    public @NonNull String TransactionId;
+    public int TagId;
+}
