9b6d0554bce5b1e652e050bcbbe0809718e12fbc
RileyPeter <rjwpeterson@csu.fullerton.edu>
2019-05-03 18:09:03 -0700

    Add files via upload

---
+++ CataloggerTest.java
@@ -0,0 +1,17 @@
+package com.savantspender.db;
+
+
+
+
+
+
+
+
+
+public class CataloggerTest extends DefaultDatabaseTest {
+    //uses tag, transaction and accont entites
+
+
+
+
+}
---
+++ DefaultDatabaseTest.java
@@ -0,0 +1,38 @@
+package com.savantspender.db;
+
+import android.content.Context;
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule;
+import androidx.room.Room;
+import androidx.test.core.app.ApplicationProvider;
+
+import com.savantspender.db.dao.EmployeeDao;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TestRule;
+
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+
+public class DefaultDatabaseTest {
+    protected AppDatabase mDatabase;
+
+    @Rule
+    public TestRule rule = new InstantTaskExecutorRule();
+
+    @Before
+    public void createDb() {
+        Context context = ApplicationProvider.getApplicationContext();
+
+        mDatabase = Room.inMemoryDatabaseBuilder(context, AppDatabase.class).build();
+    }
+
+
+    @After
+    public void closeDb() throws IOException {
+        mDatabase.close();
+    }
+}
