e43bf24cecd653d88d61963c58c15c4bd5542392
RileyPeter <rjwpeterson@csu.fullerton.edu>
2019-05-04 21:14:17 -0700

    Add files via upload

---
+++ app/src/main/java/com/savantspender/db/entity/GoalTagTrackerEntity.java
@@ -0,0 +1,34 @@
+package com.savantspender.db.entity;
+
+import androidx.annotation.NonNull;
+import androidx.room.Entity;
+import androidx.room.ForeignKey;
+import androidx.room.Index;
+
+@Entity(
+ tableName = "goaltagtacker",
+         foreignKeys = {
+@ForeignKey(
+        parentColumns = "name",
+        childColumns = "goalId",
+        entity = GoalEntity.class),
+@ForeignKey(
+        parentColumns = "id",
+        childColumns = "tagId",
+        entity = TagEntity.class)
+        },
+        indices = { @Index({"goalId"}), @Index("tagId")},
+        primaryKeys = {"goalId", "tagId"}
+)
+public class GoalTagTrackerEntity {
+    @NonNull
+    public String goalId;
+    @NonNull
+    public int tagId;
+
+    public GoalTagTrackerEntity(@NonNull String goalId,@NonNull int tagId)
+    {
+        this.goalId = goalId;
+        this.tagId = tagId;
+    }
+}
