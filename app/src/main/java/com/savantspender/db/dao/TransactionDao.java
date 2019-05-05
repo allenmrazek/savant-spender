dbc71ef406dfc4394a8e9102f9b10adeeb4bca0e
RileyPeter <rjwpeterson@csu.fullerton.edu>
2019-05-04 21:12:44 -0700

    Add files via upload

--- app/src/main/java/com/savantspender/db/dao/TransactionDao.java
+++ app/src/main/java/com/savantspender/db/dao/TransactionDao.java
@@ -1,11 +1,16 @@
 package com.savantspender.db.dao;
 
+import androidx.lifecycle.LiveData;
 import androidx.room.Dao;
 import androidx.room.Delete;
 import androidx.room.Insert;
 import androidx.room.OnConflictStrategy;
+import androidx.room.Query;
 
 import com.savantspender.db.entity.TransactionEntity;
+
+import java.util.Date;
+import java.util.List;
 
 @Dao
 public interface TransactionDao {
@@ -14,4 +19,19 @@ public interface TransactionDao {
 
     @Delete
     void delete(TransactionEntity transaction);
+
+    @Query("SELECT * FROM transactions WHERE id IN" +
+            "(SELECT transactionid FROM catalogged WHERE tagId = :tagId)") //where cattalogger tagid == tagid and transid is in table
+    LiveData<List<TransactionEntity>> getTransactionsByTagId(int tagId);
+
+    //@Query("")
+    //List<TransactionEntity> get_this_month(Date date); ///I dont know how to do type converters but this wil likely be usefull
+
+    //find by date
+
+    //find by goal
+
+    //find by no tags
+
+
 }
