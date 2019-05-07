eed229ea78e7fbfc69c0d76f271a375a1b732fb0
amrazek <amrazek@csu.fullerton.edu>
2019-05-07 03:55:51 -0700

    tags appear in list and are selectable ... but layout is wrong

--- app/src/main/java/com/savantspender/ui/frag/overview/CreateGoalFragment.java
+++ app/src/main/java/com/savantspender/ui/frag/overview/CreateGoalFragment.java
@@ -1,24 +1,107 @@
 package com.savantspender.ui.frag.overview;
 
+import android.graphics.Point;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.DialogFragment;
+import androidx.lifecycle.ViewModelProviders;
 
 import com.savantspender.R;
+import com.savantspender.db.entity.Tag;
+import com.savantspender.viewmodel.GoalsViewModel;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class CreateGoalFragment extends DialogFragment {
+    private GoalsViewModel mViewModel;
+    private TableLayout mTable;
+    private List<? extends Tag> mTags;
+
+
+    private void availableTagsChanged(List<? extends Tag> tags) {
+        mTags = tags == null ? new ArrayList<>() : tags;
+
+        buildTagTable();
+    }
+
+    private void buildTagTable() {
+        mTable.removeAllViews();
+
+
+        Point size = new Point();
+
+        getActivity().getWindow().getWindowManager().getDefaultDisplay().getSize(size);
+
+        float catWidth = getResources().getDimension(R.dimen.listitem_category_width);
+
+        long numCols = Math.round(Math.floor(size.x / catWidth));
+        long curItem = 0;
+        TableRow tr = new TableRow(getContext());
+        TableRow.LayoutParams params = new TableRow.LayoutParams(TableRow.LayoutParams.MATCH_PARENT, TableRow.LayoutParams.WRAP_CONTENT);
+
+        tr.setLayoutParams(params);
+
+
+        ArrayList<TableRow> rows = new ArrayList<>();
+
+        for (int i = 0; i < 20; ++i) { // temp to test layout
+
+
+            for (Tag tag : mTags) {
+                if (curItem % numCols == 0) {
+                    tr = new TableRow(getContext());
+                    tr.setLayoutParams(params);
+
+                    rows.add(tr);
+                }
+
+                View item = getLayoutInflater().inflate(R.layout.listitem_category, tr, false);
+
+                TextView tv = item.findViewById(R.id.txtCatName);
+                tv.setText(tag.getName());
+
+                onItemCreated(item, tag);
+
+                tr.addView(item);
+                ++curItem;
+
+            }
+
+        }
+        for (TableRow row : rows)
+            mTable.addView(row);
+    }
+
+
+    private void onItemCreated(View view, Tag tag) {
+        view.setOnClickListener(l -> {
+            l.setActivated(!l.isActivated());
+            tag.setSelected(l.isActivated());
+        });
+    }
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = LayoutInflater.from(getContext()).inflate(R.layout.dialog_creategoal, container, true);
+
         getDialog().getWindow().requestFeature(Window.FEATURE_NO_TITLE);
+
+        mTable = view.findViewById(R.id.NewGoalTagLayout);
+
+        mViewModel = ViewModelProviders.of(this, new GoalsViewModel.Factory(getActivity().getApplication())).get(GoalsViewModel.class);
+        mViewModel.availableTags().observe(getViewLifecycleOwner(), t -> availableTagsChanged(t));
 
         return view;
     }
--- app/src/main/java/com/savantspender/viewmodel/GoalsViewModel.java
+++ app/src/main/java/com/savantspender/viewmodel/GoalsViewModel.java
@@ -1,7 +1,54 @@
 package com.savantspender.viewmodel;
 
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.Transformations;
 import androidx.lifecycle.ViewModel;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.savantspender.SavantSpender;
+import com.savantspender.db.AppDatabase;
+import com.savantspender.db.entity.Tag;
+import com.savantspender.model.DataRepository;
+
+import java.util.List;
+import java.util.concurrent.Executor;
 
 public class GoalsViewModel extends ViewModel {
-    // todo
+    private final LiveData<List<? extends Tag>> mTags;
+
+    private final DataRepository mRepository;
+    private final AppDatabase mDatabase;
+    private final Executor mDiskIO;
+
+    public GoalsViewModel(DataRepository repository, AppDatabase db, Executor diskIO) {
+        mRepository = repository;
+        mDatabase = db;
+        mDiskIO = diskIO;
+
+        mTags = Transformations.map(mDatabase.tagDao().getTags(), l -> l);
+    }
+
+
+    public LiveData<List<? extends Tag>> availableTags() {
+        return mTags;
+    }
+
+    public static class Factory extends ViewModelProvider.NewInstanceFactory {
+        @NonNull
+        private final SavantSpender mApplication;
+
+        public Factory(@NonNull Application application) {
+            mApplication = (SavantSpender)application;
+        }
+
+        @Override
+        @SuppressWarnings("unchecked")
+        public <T extends ViewModel> T create(Class<T> modelClass) {
+            //noinspection unchecked
+            return (T) new GoalsViewModel(mApplication.getRepository(), mApplication.getDatabase(), mApplication.getExecutors().diskIO());
+        }
+    }
 }
